c3e2ad3024d0245cfebb1902aeae973adbcc9886
memattyoung <memattyoung@gmail.com>
2025-12-09 23:37:24 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1186,13 +1186,13 @@ if ($view === 'menu') {
                 </p>
             <?php endif; ?>
 
-            <p class="text-center small-note mt-6">
+            <p class="text-center small-note" style="margin-top:10px;">
                 Logged in as <strong><?= htmlspecialchars($empName) ?></strong>
                 (<?= htmlspecialchars($empAAA) ?>).
             </p>
 
             <?php if ($isManager): ?>
-                <div class="text-center mt-6">
+                <div class="text-center mt-10">
                     <a href="?view=manager_home" class="btn btn-secondary top-bar-btn">
                         Manager Menu
                     </a>
@@ -1245,6 +1245,7 @@ if ($view === 'menu') {
                 <div class="filters-actions">
                     <button type="submit" class="btn">Apply Filters</button>
                     <a class="btn btn-secondary" href="?view=inventory">Clear</a>
+                    <a class="btn btn-secondary" href="?view=menu">Menu</a>
                 </div>
             </form>
         </div>
@@ -1275,11 +1276,8 @@ if ($view === 'menu') {
         </div>
 
         <div class="card">
-            <div class="flex-row-between">
+            <div class="text-center mt-10">
                 <a class="btn btn-secondary top-bar-btn" href="?view=menu">Back to Battery Menu</a>
-                <?php if ($isManager): ?>
-                    <a class="btn btn-secondary top-bar-btn" href="?view=manager_home">Back to Manager Menu</a>
-                <?php endif; ?>
             </div>
         </div>
 
@@ -1336,11 +1334,8 @@ if ($view === 'menu') {
         <?php endif; ?>
 
         <div class="card">
-            <div class="flex-row-between">
+            <div class="text-center mt-10">
                 <a class="btn btn-secondary top-bar-btn" href="?view=menu">Back to Battery Menu</a>
-                <?php if ($isManager): ?>
-                    <a class="btn btn-secondary top-bar-btn" href="?view=manager_home">Back to Manager Menu</a>
-                <?php endif; ?>
             </div>
         </div>
 
@@ -1430,11 +1425,8 @@ if ($view === 'menu') {
         <?php endif; ?>
 
         <div class="card">
-            <div class="flex-row-between">
+            <div class="text-center mt-10">
                 <a class="btn btn-secondary top-bar-btn" href="?view=menu">Back to Battery Menu</a>
-                <?php if ($isManager): ?>
-                    <a class="btn btn-secondary top-bar-btn" href="?view=manager_home">Back to Manager Menu</a>
-                <?php endif; ?>
             </div>
         </div>
 
@@ -1501,11 +1493,8 @@ if ($view === 'menu') {
         <?php endif; ?>
 
         <div class="card">
-            <div class="flex-row-between">
+            <div class="text-center mt-10">
                 <a class="btn btn-secondary top-bar-btn" href="?view=menu">Back to Battery Menu</a>
-                <?php if ($isManager): ?>
-                    <a class="btn btn-secondary top-bar-btn" href="?view=manager_home">Back to Manager Menu</a>
-                <?php endif; ?>
             </div>
         </div>
 
@@ -1660,11 +1649,8 @@ if ($view === 'menu') {
         <?php endif; ?>
 
         <div class="card">
-            <div class="flex-row-between">
+            <div class="text-center mt-10">
                 <a class="btn btn-secondary top-bar-btn" href="?view=menu">Back to Battery Menu</a>
-                <?php if ($isManager): ?>
-                    <a class="btn btn-secondary top-bar-btn" href="?view=manager_home">Back to Manager Menu</a>
-                <?php endif; ?>
             </div>
         </div>
 
@@ -1707,11 +1693,8 @@ if ($view === 'menu') {
         </div>
 
         <div class="card">
-            <div class="flex-row-between">
+            <div class="text-center mt-10">
                 <a class="btn btn-secondary top-bar-btn" href="?view=menu">Back to Battery Menu</a>
-                <?php if ($isManager): ?>
-                    <a class="btn btn-secondary top-bar-btn" href="?view=manager_home">Back to Manager Menu</a>
-                <?php endif; ?>
             </div>
         </div>
 
